0f3a309ca23e1e9f63e4c1c5616ef72151540855
tac472 <tac472@gmail.com>
2025-01-02 20:07:10 +0900

    echoコマンドのプロトタイプを作成

---
+++ builtin/echo/ft_echo.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdbool.h>
+
+//TODO: エラー処理をもっと厳しくする。bash準拠のエラーメッセージを表示する。
+int ft_echo(char **args)
+{
+  size_t i = 1;
+  bool flag = false; 
+
+  if (args[i] && strcmp(args[i], "-n") == 0)
+  {
+    flag = true;
+    i++;
+  }
+  while (args[i] != NULL)
+  {
+    write(1, args[i], strlen(args[i]));
+    if (args[i + 1] != NULL)
+      write(1, " ", 1);
+    i++;
+  }
+  if (flag == false)
+    write(1, "\n", 1);
+  return 0;
+}
+
+int main(int argc, char **argv)
+{
+  ft_echo(argv);
+  return 0;
+}
